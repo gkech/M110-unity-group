21f8252ba2e366378844e16b11e0543850fb51d6
VagelisN <vagelisn23@gmail.com>
2023-02-15 20:24:52 +0200

    keep previous zone in hanoi puzzle

--- Assets/Scripts/HanoiZoneController.cs
+++ Assets/Scripts/HanoiZoneController.cs
@@ -9,28 +9,48 @@ public class HanoiZoneController : MonoBehaviour
     public GameObject zoneAbove;
     public GameObject discAttached;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     private void OnTriggerEnter(Collider other) {
-        if(other.gameObject.tag == "Disc") {
-            if (zoneAbove != null)
-                zoneAbove.SetActive(true);
+        if(other.gameObject.tag == "Disc") 
+        {
+            if (discCanBePlaced(other.gameObject)) {
+                discAttached = other.gameObject;
 
-            if (zoneBellow != null)
-                zoneBellow.SetActive(false);
+                // Set the disc to return to this zone if not snapped elsewhere
+                discAttached.GetComponent<SnapInteractor>()
+                    .InjectOptionalTimeOutInteractable(this.gameObject.GetComponent<SnapInteractable>());
 
-            discAttached = other.gameObject;
-            discAttached.GetComponent<SnapInteractor>().InjectOptionalTimeOutInteractable(this.gameObject.GetComponent<SnapInteractable>());
+                if (zoneAbove != null)
+                    zoneAbove.SetActive(true);
+
+                if (zoneBellow != null)
+                    zoneBellow.SetActive(false);
+            }
+
+
+            /*if(discAttached == null) 
+            {
+                discAttached = other.gameObject;
+                if (zoneBellow != null && 
+                    discAttached.layer > zoneBellow.GetComponent<HanoiZoneController>().discAttached.layer)
+                {
+                    Debug.Log("EEEEEEE");
+                    this.gameObject.SetActive(false);
+                    return;
+                }
+
+                if (zoneAbove != null)
+                    zoneAbove.SetActive(true);
+
+                if (zoneBellow != null)
+                    zoneBellow.SetActive(false);
+
+                // Set the disc to return to this zone if not snapped elsewhere
+                discAttached.GetComponent<SnapInteractor>()
+                    .InjectOptionalTimeOutInteractable(this.gameObject.GetComponent<SnapInteractable>());
+
+                discAttached.GetComponent<PreviousZone>().previousSnapZone 
+                = this.gameObject.transform;
+            }*/
         }
     }
 
@@ -44,5 +64,23 @@ public class HanoiZoneController : MonoBehaviour
 
             discAttached = null;
         }
+    }
+
+    /* 
+     * A disc can be placed if there is no zone bellow or if the disc on the zone bellow 
+     * is bigger than the current disc
+     */
+    private bool discCanBePlaced(GameObject disc) 
+    {
+        if (discAttached == null) 
+        {
+            if (zoneBellow != null && 
+            disc.layer > zoneBellow.GetComponent<HanoiZoneController>().discAttached.layer)
+            {
+                return false;
+            }
+            return true;
+        }
+        return false;
     }
 }
---
+++ Assets/Scripts/PreviousZone.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PreviousZone : MonoBehaviour
+{
+    public Transform previousSnapZone = null;
+}
