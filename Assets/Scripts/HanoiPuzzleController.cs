752e1f3e1bcb224242ee0eb17cb84ede985ad20f
VagelisN <vagelisn23@gmail.com>
2023-02-21 23:31:37 +0200

    almost not final

--- Assets/Scripts/HanoiPuzzleController.cs
+++ Assets/Scripts/HanoiPuzzleController.cs
@@ -8,6 +8,12 @@ public class HanoiPuzzleController : MonoBehaviour
     public GameObject bottomZone;
     public GameObject middleZone;
     public GameObject topZone;
+    public GameObject key;
+    public ParticleSystem smoke;
+
+    public GameObject zones;
+
+    private bool hasPlayed = false;
 
     private GameObject bottomZoneDiscAttached;
     private GameObject middleZoneDiscAttached;
@@ -21,13 +27,29 @@ public class HanoiPuzzleController : MonoBehaviour
         middleZoneDiscAttached = middleZone.GetComponent<HanoiZoneController>().discAttached;
         topZoneDiscAttached = topZone.GetComponent<HanoiZoneController>().discAttached;
 
-        if (bottomZoneDiscAttached != null &&
-            middleZoneDiscAttached != null &&
-            topZoneDiscAttached != null &&
-            bottomZoneDiscAttached.name == "DiscBig" && 
-            middleZoneDiscAttached.name == "DiscMedium" && 
-            topZoneDiscAttached.name == "DiscSmall"
-        )
+        
+        
+        // if (bottomZoneDiscAttached != null &&
+        //     middleZoneDiscAttached != null &&
+        //     topZoneDiscAttached != null &&
+        //     bottomZoneDiscAttached.name == "DiscBig" && 
+        //     middleZoneDiscAttached.name == "DiscMedium" && 
+        //     topZoneDiscAttached.name == "DiscSmall"
+        // )
+        if( bottomZoneDiscAttached != null && bottomZoneDiscAttached.name == "DiscSmall" )
+        {
             Debug.Log("WIN");
+            
+            if(  !hasPlayed )
+            {   
+                zones.SetActive(!zones.activeSelf);
+                key.SetActive(!key.activeSelf);
+                smoke.Play();
+                hasPlayed = true;
+            }
+            
+
+        }
+
     }
 }
