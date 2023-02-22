9b6a4fbb72e591063cb249a1a9a927b3d83e18fb
Vaggelis Neamonitis <vaggelis.neamonitis@theblueground.com>
2023-02-22 19:55:08 +0200

    change scene

---
+++ Assets/Scripts/LoadScenePast.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadScenePast : MonoBehaviour
+{
+    public void changeScene()
+    {
+        SceneManager.LoadScene("PastScene");
+    }
+}
